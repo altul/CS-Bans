362d73aef8647058b748153c9d16eb258b4e4a97
SkuLL <gheonea_iulian@yahoo.com>
2019-08-23 13:45:35 +0300

    Update mainmenu.php

--- themes/default/views/admin/mainmenu.php
+++ themes/default/views/admin/mainmenu.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Верхнее меню админцентра
+ * Top menu of the admin center
  */
 
 /**
@@ -13,8 +13,8 @@
  */
 
 /**
- * @param string $active Определяет активный раздел меню
- * @param string $activeиет Определяет активную кнопку
+ * @param string $active Defines the active menu section.
+ * @param string $activeиет Defines the active button.
  */
 $disabled = ' disabled="disabled" onclick="return false;"';
 $this->widget('bootstrap.widgets.TbTabs', array(
@@ -22,38 +22,38 @@ $this->widget('bootstrap.widgets.TbTabs', array(
 	'placement' => 'above',
 	'tabs' => array(
 		array(
-			'label' => 'Админцентр',
+			'label' => 'Admin center',
 			'content' => '
 				<ul class="inline">
-					<li><a href="' . Yii::app()->createUrl('/admin/index') . '" class="btn"'.($activebtn == 'admsystem' ? $disabled : '').'>Информация о системе</a></li>
-					<li><a href="' . Yii::app()->createUrl('/Bans/create') . '" class="btn"'.($activebtn == 'admaddban' ? $disabled : '').'>Добавить бан</a></li>
-					<li><a href="' . Yii::app()->createUrl('/admin/addbanonline') . '" class="btn"'.($activebtn == 'addbanonline' ? $disabled : '').'>Добавить бан онлайн</a></li>
+					<li><a href="' . Yii::app()->createUrl('/admin/index') . '" class="btn"'.($activebtn == 'admsystem' ? $disabled : '').'>System Information</a></li>
+					<li><a href="' . Yii::app()->createUrl('/Bans/create') . '" class="btn"'.($activebtn == 'admaddban' ? $disabled : '').'>Add ban</a></li>
+					<li><a href="' . Yii::app()->createUrl('/admin/addbanonline') . '" class="btn"'.($activebtn == 'addbanonline' ? $disabled : '').'>Add Ban Online</a></li>
 				</ul>',
 			'active' => $active == 'main'
 		),
 		array(
-			'label' => 'Сервер',
+			'label' => 'Server',
 			'content' => '
 				<ul class="inline">
-					<li><a href="' . Yii::app()->createUrl('/serverinfo/admin') . '" class="btn"'.($activebtn == 'servsettings' ? $disabled : '').'>Настройки</a></li>
-					<li><a href="' . Yii::app()->createUrl('/admin/reasons') . '" class="btn"'.($activebtn == 'servreasons' ? $disabled : '').'>Причины банов</a></li>
-					<li><a href="' . Yii::app()->createUrl('/amxadmins/admin') . '" class="btn"'.($activebtn == 'servamxadmins' ? $disabled : '').'>Админы</a></li>
-					<li><a href="' . Yii::app()->createUrl('/amxadmins/adminsonservers') . '" class="btn"'.($activebtn == 'servadmassign' ? $disabled : '').'>Назначение админов</a></li>
+					<li><a href="' . Yii::app()->createUrl('/serverinfo/admin') . '" class="btn"'.($activebtn == 'servsettings' ? $disabled : '').'>Settings</a></li>
+					<li><a href="' . Yii::app()->createUrl('/admin/reasons') . '" class="btn"'.($activebtn == 'servreasons' ? $disabled : '').'>Causes of Bans</a></li>
+					<li><a href="' . Yii::app()->createUrl('/amxadmins/admin') . '" class="btn"'.($activebtn == 'servamxadmins' ? $disabled : '').'>Admins</a></li>
+					<li><a href="' . Yii::app()->createUrl('/amxadmins/adminsonservers') . '" class="btn"'.($activebtn == 'servadmassign' ? $disabled : '').'>Admin Assignment</a></li>
 				</ul>',
 			'active' => $active == 'server'
 		),
 		array(
-			'label' => 'Веб сайт',
+			'label' => 'Web Site',
 			'content' => '
 				<ul class="inline">
-					<li><a href="' . Yii::app()->createUrl('/webadmins/admin') . '" class="btn"'.($activebtn == 'webadmins' ? $disabled : '').'>ВЕБ админы</a></li>
-					<li><a href="' . Yii::app()->createUrl('/levels/admin') . '" class="btn"'.($activebtn == 'webadmlevel' ? $disabled : '').'>Уровни</a></li>
-					<li><a href="' . Yii::app()->createUrl('/usermenu/admin') . '" class="btn"'.($activebtn == 'webmainmenu' ? $disabled : '').'>Ссылки</a></li>
-					<li><a href="' . Yii::app()->createUrl('/admin/websettings') . '" class="btn"'.($activebtn == 'websettings' ? $disabled : '').'>Настройки</a></li>
-					<li><a href="' . Yii::app()->createUrl('/logs/admin') . '" class="btn"'.($activebtn == 'logs' ? $disabled : '').'>Логи</a></li>
+					<li><a href="' . Yii::app()->createUrl('/webadmins/admin') . '" class="btn"'.($activebtn == 'webadmins' ? $disabled : '').'>Web Admins</a></li>
+					<li><a href="' . Yii::app()->createUrl('/levels/admin') . '" class="btn"'.($activebtn == 'webadmlevel' ? $disabled : '').'>Levels</a></li>
+					<li><a href="' . Yii::app()->createUrl('/usermenu/admin') . '" class="btn"'.($activebtn == 'webmainmenu' ? $disabled : '').'>References</a></li>
+					<li><a href="' . Yii::app()->createUrl('/admin/websettings') . '" class="btn"'.($activebtn == 'websettings' ? $disabled : '').'>Settings</a></li>
+					<li><a href="' . Yii::app()->createUrl('/logs/admin') . '" class="btn"'.($activebtn == 'logs' ? $disabled : '').'>Logs</a></li>
 					'.(Yii::app()->hasModule('billing')
 						?
-					'<li><a href="'.Yii::app()->createUrl('/billing/tariff/admin').'" class="btn"'.($activebtn == 'tariffs' ? $disabled : '').'>Тарифы</a></li>'
+					'<li><a href="'.Yii::app()->createUrl('/billing/tariff/admin').'" class="btn"'.($activebtn == 'tariffs' ? $disabled : '').'>Tariffs</a></li>'
 						:
 					'').'
 				</ul>',
