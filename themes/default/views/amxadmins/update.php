6f582943f0addca4d5df2923121bd88e911a1e69
SkuLL <gheonea_iulian@yahoo.com>
2019-08-27 21:29:44 +0300

    Update update.php

--- themes/default/views/amxadmins/update.php
+++ themes/default/views/amxadmins/update.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Вьюшка редактирования админа серверов
+ * Admin server editing view
  */
 
 /**
@@ -12,21 +12,21 @@
  * @license http://creativecommons.org/licenses/by-nc-sa/4.0/deed.ru  «Attribution-NonCommercial-ShareAlike»
  */
 
-$this->pageTitle = Yii::app()->name . ' :: Админцентр - Редактировать админа';
+$this->pageTitle = Yii::app()->name . ' :: Admin Center - Edit admin';
 $this->breadcrumbs = array(
-	'Админцентр' => array('/admin/index'),
-	'AmxModX админы' => array('admin'),
-	'Редактировать админа ' . $model->nickname
+	'Admin Center' => array('/admin/index'),
+	'AmxModX Admins' => array('admin'),
+	'Edit Admin ' . $model->nickname
 );
 
 $this->renderPartial('/admin/mainmenu', array('active' =>'server', 'activebtn' => 'servamxadmins'));
 
 $this->menu=array(
-	array('label'=>'Добавить AmxModX админа', 'url'=>array('create')),
-	array('label'=>'Управление AmxModX админами', 'url'=>array('admin')),
+	array('label'=>'Add AmxModX Admin', 'url'=>array('create')),
+	array('label'=>'Managing AmxModX Admins', 'url'=>array('admin')),
 );
 ?>
 
-<h2>Редактировать AmxModX админа <?php echo $model->nickname; ?></h2>
+<h2>Edit AmxModX Admin <?php echo $model->nickname; ?></h2>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
